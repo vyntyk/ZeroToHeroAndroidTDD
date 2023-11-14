3d86dd4e7af60e927d02a36560214693a3efe944
Viktor Belous <vyntyk@yandex.ru>
2023-11-14 18:41:10 +0300

    task/009-remove-textview solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,40 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var linearLayout: LinearLayout
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        linearLayout = findViewById(R.id.rootLayout)
+        val button = findViewById<Button>(R.id.removeButton)
+        textView = findViewById(R.id.titleTextView)
+
+        button.setOnClickListener {
+            linearLayout.removeView(textView)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val removeView = linearLayout.childCount == 1
+        outState.putBoolean("key", removeView)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val removeView = savedInstanceState.getBoolean("key")
+        if(removeView)
+            linearLayout.removeView(textView)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
