08b0cae3d4bbb19d44d62bd06f80a455ffb103d6
Viktor Belous <vyntyk@yandex.ru>
2023-11-15 15:58:31 +0300

    task/012-increment-max solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,43 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    private var uiState:UiState = UiState.Base("0")
+    private val count = Count.Base(2, 4)
+    private lateinit var textView: TextView
+    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.incrementButton)
+        textView = findViewById(R.id.countTextView)
+
+        button.setOnClickListener {
+            uiState = count.increment(textView.text.toString())
+            uiState.apply(textView, button)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable("key", uiState)
+    }
+
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        uiState = savedInstanceState.getSerializable("key") as UiState
+        uiState.apply(textView, button)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/UiState.kt
@@ -0,0 +1,24 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import android.widget.Button
+import android.widget.TextView
+import java.io.Serializable
+
+interface UiState : Serializable{
+    fun apply(textView: TextView, button: Button)
+
+    data class Base(private val text: String) : UiState {
+
+        override fun apply(textView: TextView, button: Button) {
+            textView.text = text
+        }
+
+    }
+    data class Max(private val text: String) : UiState {
+        override fun apply(textView: TextView, button: Button) {
+            textView.text = text
+            button.isEnabled = false
+        }
+    }
+}
+
