46591cfd6820c94d1c4577d1bbef7b6dd05c4483
Viktor Belous <vyntyk@yandex.ru>
2023-11-19 00:07:21 +0300

    part 2

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainViewModel.kt
@@ -0,0 +1,30 @@
+package ru.easycode.zerotoheroandroidtdd
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+
+class MainViewModel(
+    private val liveDataWrapper: LiveDataWrapper,
+    private val repository: Repository,
+) {
+    private val viewModelScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
+    fun liveData() = liveDataWrapper.liveData()
+    fun load() {
+        liveDataWrapper.update(UiState.ShowProgress)
+        viewModelScope.launch {
+            repository.load()
+            liveDataWrapper.update(UiState.ShowData)
+        }
+    }
+
+    fun save(bundleWrapper: BundleWrapper.Save) {
+        liveDataWrapper.save(bundleWrapper)
+    }
+
+    fun restore(bundleWrapper: BundleWrapper.Restore) {
+        val uiState = bundleWrapper.restore()
+        liveDataWrapper.update(uiState)
+    }
+
+}
