d89571465426d83ff3cf37eac42091e80779fd4e
Viktor Belous <vyntyk@yandex.ru>
2023-11-18 15:14:42 +0300

    task/014-progress solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,32 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var titleTextView: TextView
+    private lateinit var actionButton: Button
+    private lateinit var progressBar: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        titleTextView = findViewById(R.id.titleTextView)
+        actionButton = findViewById(R.id.actionButton)
+        progressBar = findViewById(R.id.progressBar)
+
+        actionButton.setOnClickListener{
+            actionButton.isEnabled = false
+            progressBar.visibility = View.VISIBLE
+
+            actionButton.postDelayed({
+                titleTextView.visibility = View.VISIBLE
+                progressBar.visibility = View.GONE
+                actionButton.isEnabled = true
+            }, 3500)
+        }
     }
 }
