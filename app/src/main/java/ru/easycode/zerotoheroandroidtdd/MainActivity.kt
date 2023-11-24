78c8de550eec7e7cbfa4630a054692ce5dbab5d5
Viktor Belous <vyntyk@yandex.ru>
2023-11-24 20:04:53 +0300

    task/022-list готово

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,50 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputEditText
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val textList = ArrayList<String>()
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.actionButton.setOnClickListener {
+            val inputText = binding.inputEditText.text
+            textList.add(inputText.toString())
+            val textView = TextView(this).apply {
+                text = inputText
+            }
+            binding.contentLayout.addView(textView)
+            binding.inputEditText.text?.clear()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putStringArrayList(KEY, textList)
+    }
+
+    companion object {
+        private const val KEY = "list"
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val list = savedInstanceState.getStringArrayList(KEY) ?: ArrayList()
+        textList.addAll(list)
+        textList.forEach { inputText ->
+            val textView = TextView(this).apply {
+                text = inputText
+            }
+            binding.contentLayout.addView(textView)
+        }
     }
 }
