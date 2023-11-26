7705ecbd19627edc6c36b914f3deddc4e7285987
Viktor Belous <vyntyk@yandex.ru>
2023-11-26 22:48:21 +0300

    task/025-navigation fix

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/list/ListFragment.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/list/ListFragment.kt
@@ -18,15 +18,16 @@ class ListFragment : AbstractFragment<FragmentListBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val viewModel = (activity as ProvideViewModel).viewModel(ListViewModel::class.java)
+        viewModel = (activity as ProvideViewModel).viewModel(ListViewModel::class.java)
 
         binding.addButton.setOnClickListener {
             viewModel.create()
         }
+
         val adapter = Adapter()
         binding.recyclerView.adapter = adapter
 
-        viewModel.liveData().observe(viewLifecycleOwner){
+        viewModel.liveData().observe(viewLifecycleOwner) {
             adapter.update(it)
         }
         savedInstanceState?.let {
@@ -37,8 +38,5 @@ class ListFragment : AbstractFragment<FragmentListBinding>() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         viewModel.save(BundleWrapper.Base(outState))
-    }
-    override fun onViewStateRestored(savedInstanceState: Bundle?) {
-        super.onViewStateRestored(savedInstanceState)
     }
 }
