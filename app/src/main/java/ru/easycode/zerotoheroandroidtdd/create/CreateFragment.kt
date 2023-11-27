374f382701f52602f68922eb34523608e6b7dc45
Viktor Belous <vyntyk@yandex.ru>
2023-11-27 14:39:49 +0300

    task/025-navigation fix2

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/create/CreateFragment.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/create/CreateFragment.kt
@@ -9,6 +9,7 @@ import android.view.inputmethod.InputMethod
 import android.view.inputmethod.InputMethodManager
 import androidx.activity.OnBackPressedCallback
 import androidx.core.widget.addTextChangedListener
+import com.google.android.material.internal.ViewUtils.hideKeyboard
 import ru.easycode.zerotoheroandroidtdd.core.AbstractFragment
 import ru.easycode.zerotoheroandroidtdd.core.ProvideViewModel
 import ru.easycode.zerotoheroandroidtdd.databinding.FragmentCreateBinding
@@ -17,20 +18,31 @@ class CreateFragment : AbstractFragment<FragmentCreateBinding>() {
     override fun bind(inflater: LayoutInflater, container: ViewGroup?) =
         FragmentCreateBinding.inflate(inflater, container, false)
 
+    private lateinit var viewModel: CreateViewModel
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() = viewModel.comeback()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val viewModel = (activity as ProvideViewModel).viewModel(CreateViewModel::class.java)
-        requireActivity().onBackPressedDispatcher.addCallback(
-            object : OnBackPressedCallback(true) {
-                override fun handleOnBackPressed() = viewModel.comeback()
-            })
+        viewModel = (activity as ProvideViewModel).viewModel(CreateViewModel::class.java)
+
+        requireActivity().onBackPressedDispatcher.addCallback(onBackPressedCallback)
+
 
         binding.inputEditText.addTextChangedListener {
             binding.createButton.isEnabled = binding.inputEditText.text.toString().length >= 3
         }
+
         binding.createButton.setOnClickListener {
             hideKeyboard()
             viewModel.add(binding.inputEditText.text.toString())
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        onBackPressedCallback.remove()
     }
 }
